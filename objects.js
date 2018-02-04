8e73cca1e269f1d6d941ae2c25ce0c0d0ac34969
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 04:24:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({artistName: ""});
 
 function updatePlaylist(playlist, atistName, songTitle){
-  return Object.assign({}, playlist, {[artistName]: songTitle})
+  return Object.assign(playlist, {[artistName]: songTitle})
 }

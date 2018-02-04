fd5b5b0bef219aa7f2f939569fcf00e9ffffa619
Jeffrey Damaso <jeffreydamaso340@gmail.com>
2018-02-04 04:35:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,4 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle){
    return Object.assign(playlist, {[artistName]: songTitle})
 }
 
-function 
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist
+}
